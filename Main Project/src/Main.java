2180bcadd780d958b4c57322cf55874e54414e2d
Krip4yk-main <43015491+Krip4yk-main@users.noreply.github.com>
2019-10-20 09:09:45 +0300

    changes2

--- Main Project/src/Main.java
+++ Main Project/src/Main.java
@@ -2,6 +2,8 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -38,16 +40,47 @@ public class Main {
             System.out.println("Create sideBarElements...");
             int n_sb = 10;
             SideBarElements sideBarElements[] = new SideBarElements[n_sb];
-            sideBarElements[0] = new SideBarElements(0, "Home", "data\\25694.png");
-            sideBarElements[1] = new SideBarElements(1, "Categories", "data\\img_574467.png");
-            sideBarElements[2] = new SideBarElements(2, "Library", "data\\115-512.png");
-            sideBarElements[3] = new SideBarElements(3, "Activity", "data\\activity_execution_416666.png");
-            sideBarElements[4] = new SideBarElements(4, "Projects", "data\\590-512.png");
+            sideBarElements[0] = new SideBarElements(0, "Home", "data\\1.png");
+            sideBarElements[1] = new SideBarElements(1, "Categories", "data\\2.png");
+            sideBarElements[2] = new SideBarElements(2, "Library", "data\\3.png");
+            sideBarElements[3] = new SideBarElements(3, "Activity", "data\\4.png");
+            sideBarElements[4] = new SideBarElements(4, "Projects", "data\\5.png");
             sideBarElements[5] = new SideBarElements(5);
             sideBarElements[6] = new SideBarElements(6);
             sideBarElements[7] = new SideBarElements(7);
             sideBarElements[8] = new SideBarElements(8);
-            sideBarElements[9] = new SideBarElements(9, "Settings", "data\\setting-512.png");
+            sideBarElements[9] = new SideBarElements(9, "Settings", "data\\9.png");
+
+            for (int i = 0; i < n_sb; i++) {
+                int finalI = i;
+                sideBarElements[i].panel.addMouseListener(new MouseListener() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        click_sidebar(e, finalI);
+                    }
+
+                    @Override
+                    public void mousePressed(MouseEvent e) {
+
+                    }
+
+                    @Override
+                    public void mouseReleased(MouseEvent e) {
+
+                    }
+
+                    @Override
+                    public void mouseEntered(MouseEvent e) {
+
+                    }
+
+                    @Override
+                    public void mouseExited(MouseEvent e) {
+
+                    }
+                });
+            }
+
             System.out.println("Add sideBarElements...");
             for (int i = 0; i < n_sb; i++) {
                 sidebar.add(sideBarElements[i].panel);
@@ -55,12 +88,53 @@ public class Main {
         }
 
         {
+            setHome();
+        } // home
+
+        {
             setCategories();
 
+            int n_c = 10;
+            CategoriesElements categoriesElements[] = new CategoriesElements[n_c];
+            categoriesElements[0] = new CategoriesElements(0, "History");
+            categoriesElements[1] = new CategoriesElements(1, "Tech");
+            categoriesElements[2] = new CategoriesElements(2, "Technology");
+            categoriesElements[3] = new CategoriesElements(3, "Guides");
+            categoriesElements[4] = new CategoriesElements(4, "Patents");
+            categoriesElements[5] = new CategoriesElements(5, "Patterns");
+            categoriesElements[6] = new CategoriesElements(6, "Famous People");
+            categoriesElements[7] = new CategoriesElements(7, "Theoretic");
+            categoriesElements[8] = new CategoriesElements(8, "Research");
+            categoriesElements[9] = new CategoriesElements(9, "Other");
 
-            active.add(categories);
+            for (int i = 0; i < n_c; i++) {
+                categories.add(categoriesElements[i].panel);
+            }
+
         } // catefories
 
+        {
+            setLibrary();
+        } // library
+
+        {
+            setActivity();
+        } // activity
+
+        {
+            setProjects();
+        } // projects
+
+        {
+            setSettings();
+        } // settings
+
+        active.add(home);
+        active.add(categories);
+        active.add(library);
+        active.add(activity);
+        active.add(projects);
+        active.add(settings);
         {
             System.out.println("Add panels|elements...");
             corePanel.add(sidebar);
@@ -100,38 +174,143 @@ public class Main {
     }
     private static void setHome() {
         home = new JPanel();
+        home.setVisible(true);
         home.setSize(size_active);
         home.setLocation(0, 0);
+        home.setLayout(null);
     }
     private static void setCategories() {
         categories = new JPanel();
         categories.setVisible(false);
         categories.setSize(size_active);
         categories.setLocation(0, 0);
+        categories.setLayout(null);
     }
     private static void setLibrary() {
         library = new JPanel();
         library.setVisible(false);
         library.setSize(size_active);
         library.setLocation(0, 0);
+        library.setLayout(null);
     }
     private static void setActivity() {
         activity = new JPanel();
         activity.setVisible(false);
         activity.setSize(size_active);
         activity.setLocation(0, 0);
+        activity.setLayout(null);
     }
     private static void setProjects() {
         projects = new JPanel();
         projects.setVisible(false);
         projects.setSize(size_active);
         projects.setLocation(0, 0);
+        projects.setLayout(null);
     }
     private static void setSettings() {
         settings = new JPanel();
         settings.setVisible(false);
         settings.setSize(size_active);
         settings.setLocation(0, 0);
+        settings.setLayout(null);
+    }
+    private static void click_sidebar(MouseEvent me, int index) {
+        if (act == index) return;
+        closeAct(act);
+        act = index;
+        switch (index) {
+            case 0: {
+                home.setVisible(true);
+                break;
+            }
+            case 1: {
+                categories.setVisible(true);
+                break;
+            }
+            case 2: {
+                library.setVisible(true);
+                break;
+            }
+            case 3: {
+                activity.setVisible(true);
+                break;
+            }
+            case 4: {
+                projects.setVisible(true);
+                break;
+            }
+            case 5: {
+                home.setVisible(true);
+                act = index;
+                break;
+            }
+            case 6: {
+                home.setVisible(true);
+                act = index;
+                break;
+            }
+            case 7: {
+                home.setVisible(true);
+                act = index;
+                break;
+            }
+            case 8: {
+                home.setVisible(true);
+                act = index;
+                break;
+            }
+            case 9: {
+                settings.setVisible(true);
+                break;
+            }
+            default: {
+                System.out.println("(click_sidebar) Out of range...");
+            }
+        }
+    }
+    private static void closeAct(int index) {
+        switch (index) {
+            case 0: {
+                home.setVisible(false);
+                break;
+            }
+            case 1: {
+                categories.setVisible(false);
+                break;
+            }
+            case 2: {
+                library.setVisible(false);
+                break;
+            }
+            case 3: {
+                activity.setVisible(false);
+                break;
+            }
+            case 4: {
+                projects.setVisible(false);
+                break;
+            }
+            case 5: {
+                home.setVisible(false);
+                break;
+            }
+            case 6: {
+                home.setVisible(false);
+                break;
+            }
+            case 7: {
+                home.setVisible(false);
+                break;
+            }
+            case 8: {
+                home.setVisible(false);
+                break;
+            }
+            case 9: {
+                settings.setVisible(false);
+                break;
+            }
+        }
     }
 }
 class SideBarElements {
@@ -157,17 +336,15 @@ class SideBarElements {
         try {
             b_image = ImageIO.read(new File(img));
         } catch (IOException e) {}
-        b_image = resize(b_image, 44, 44);
 
         image = new JLabel(new ImageIcon(b_image));
 
         image.setSize(44, 44);
         image.setLocation(14, 14);
-        //image.setBounds(14, 14, 44, 44);
 
         text.setSize(188, 44);
         text.setLocation(72, 14);
-        //text.setBounds(72, 14, 188, 44);
+        text.setFont(new Font(Font.SERIF, 0, 21));
 
         panel.setSize(size);
         panel.setLocation(location);
@@ -186,34 +363,52 @@ class SideBarElements {
 
         panel.setSize(size);
         panel.setLocation(location);
-        panel.setBackground(Color.blue);
-    }
-
-    public static BufferedImage resize(BufferedImage img, int newW, int newH) {
-        int w = img.getWidth();
-        int h = img.getHeight();
-        BufferedImage dimg = new BufferedImage(newW, newH, img.getType());
-        Graphics2D g = dimg.createGraphics();
-        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-                RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        g.drawImage(img, 0, 0, newW, newH, 0, 0, w, h, null);
-        g.dispose();
-        return dimg;
+        panel.setVisible(false);
     }
 }
 class CategoriesElements {
     public JPanel panel;
-    public JPanel contais;
+    public JPanel contains;
     public JLabel title;
     public JLabel text;
     public int index;
 
+    public CategoriesElements(int index, String title) {
+        panel = new JPanel();
+        contains = new JPanel();
+        this.title = new JLabel(title);
+        this.index = index;
+
+        this.title.setLocation(14, 14);
+        this.title.setSize(996, 44);
+        this.title.setFont(new Font(Font.SERIF, Font.PLAIN, 21));
+
+        panel.setLayout(null);
+        panel.setSize(1020, 72);
+        panel.setLocation(0, index*72);
+        panel.setBorder(new LineBorder(Color.black));
+
+        panel.setBackground(Color.red);
+
+        panel.add(this.title);
+    }
     public CategoriesElements(int index, String title, String text, String img) {
         panel = new JPanel();
-        contais = new JPanel();
+        contains = new JPanel();
         this.title = new JLabel(title);
         this.text = new JLabel(text);
         this.index = index;
 
+        this.title.setLocation(14, 14);
+        this.title.setSize(996, 44);
+        this.title.setFont(new Font(Font.SERIF, Font.PLAIN, 21));
+
+        panel.setLayout(null);
+        panel.setSize(1020, 72);
+        panel.setLocation(0, index*72);
+
+        panel.setBackground(Color.red);
+
+        panel.add(this.title);
     }
 }
